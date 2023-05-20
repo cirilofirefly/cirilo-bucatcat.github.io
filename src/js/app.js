6e53db83fe33ab6fb1af9e8f359861c8e33ba6ef
Cirilo Bucatcat Jr <bucatcat1999@gmail.com>
2023-05-20 13:26:27 +0800

    sidebar and darkmode toggle

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 
+const btnTheme = document.getElementById('btnTheme');
+btnTheme.addEventListener('click', toggleTheme);
+
 window.addEventListener('load', function() {
     getThemeMode();
     setTimeout(() => loaded(), 1000);
@@ -11,8 +14,28 @@ function loaded() {
 
 function getThemeMode() {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark')
+        document.documentElement.classList.add('dark');
+        btnTheme.innerHTML += getThemeIcon('moon');
     } else {
         document.documentElement.classList.remove('dark');
+        btnTheme.innerHTML += getThemeIcon('sun');
     }
+}
+
+function toggleTheme() {
+    btnTheme.innerHTML = '';
+    let isDarkMode = localStorage.theme == 'dark'
+    if(isDarkMode) {
+        localStorage.setItem('theme', 'light');
+        btnTheme.innerHTML += getThemeIcon('sun');
+        document.documentElement.classList.remove('dark');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        btnTheme.innerHTML += getThemeIcon('moon');
+        document.documentElement.classList.add('dark');
+    }
+}
+
+function getThemeIcon(icon) {
+    return '<i class="fas fa-' + icon + '"></i>';
 }
