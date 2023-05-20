4218814b169a04c28da56ce4ea5c1fb565343b32
Cirilo Bucatcat Jr <bucatcat1999@gmail.com>
2023-05-20 10:10:48 +0800

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('load', function() {
+    getThemeMode();
+    loaded();
+});
+
+function loaded() {
+    let loader = document.querySelector('.loader');
+    loader.classList.add('loaded');
+}
+
+function getThemeMode() {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+}
