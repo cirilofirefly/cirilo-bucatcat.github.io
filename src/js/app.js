a18c6a1042ec3cc5fd9e24e0e3e9adcabb77c351
Cirilo Bucatcat Jr <bucatcat1999@gmail.com>
2023-07-12 08:18:14 +0800

    email function for contact form

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ const brand = document.getElementById('brand');
 const popupVideo = document.getElementById('popup-video');
 const projectVideos = document.querySelectorAll('#project-video');
 const closePopUpVideo = document.querySelector('#close-popup-video');
+const contactForm = document.getElementById('contactForm');
+const successAlert = document.getElementById('success-alert');
 
 const transformHamburger = (isToggled) => {
     const currentColor = isToggled ? 'bg-primary' : 'bg-quaternary';
@@ -14,6 +16,10 @@ const transformHamburger = (isToggled) => {
     hamburgerLines.forEach((hamburgerLine) => {
         hamburgerLine.classList.replace(currentColor, newColor)
     });
+}
+
+const sendEmail = (templateParams) => {
+    return emailjs.send('service_xdygofk','template_egm8gwj', templateParams, 'tlrutdeEOohZQl8YI')
 }
 
 mobileNavBtn.addEventListener('click', function() {
@@ -35,9 +41,35 @@ closePopUpVideo.addEventListener('click', () => {
     popupVideo.classList.replace('flex', 'hidden');
 })
 
+contactForm.addEventListener('submit', (e) => {
 
+    e.preventDefault();
 
+    let name = getElementValue('name');
+    let email = getElementValue('email');
+    let subject = getElementValue('subject');
+    let message = getElementValue('message');
+    let templateParams = { to_name: 'Cirilo Bucatcat Jr.', name, email, subject, message }
 
+    sendEmail(templateParams) 
+        .then((response) => {
+            successAlert.classList.replace('hidden', 'block')
+            Object.keys(templateParams).forEach((key) => {
+                clearElementValue(key)
+            })
+            setTimeout(() => {
 
+                successAlert.classList.replace('block', 'hidden')
+            }, 3000)
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+})
 
+function clearElementValue(id) {
+    if(document.getElementById(id)) document.getElementById(id).value = ''
+}
 
+function getElementValue(id) {
+    return document.getElementById(id).value ?? ''
+}
